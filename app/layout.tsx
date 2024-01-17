a5a05c0a3f20a075ad61da97fc0daf6b43279b68
Esther <itolimaesther@gmail.com>
2024-01-17 02:29:47 +0000

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const nunito = Nunito_Sans({ subsets: ["latin"], weight: ["400", "700"] });
+const nunito = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Vendo Store",
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	images: {
 		domains: ["cdn.imagin.studio"],
 	},
-	assetPrefix: './',
+	assetPrefix: '/.',
 	output: 'export',
 };
 
